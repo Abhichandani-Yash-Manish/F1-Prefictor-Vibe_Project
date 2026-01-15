48bbb559e538550b42abfe4864145d58115b6778
abhichandani_yash <yashraj2507@gmail.com>
2026-01-15 22:38:45 +0530

    Fix Script Prop: Remove async attribute from AdSense Script

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -69,7 +69,6 @@ export default function RootLayout({
         {/* Google AdSense Script */}
         {process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID && (
           <Script
-            async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
             crossOrigin="anonymous"
             strategy="afterInteractive"
